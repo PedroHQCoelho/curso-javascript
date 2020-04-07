7ad92f46988594856926b62d19ce494d1654068c
PedroHQCoelho <pedrohqcoelho@gmail.com>
2020-04-07 18:44:40 -0300

    Curso da Udemy

---
+++ Curso Udemy/script.js
@@ -0,0 +1,10 @@
+function valida(){
+    var nome = document.getElementById('nome');
+
+    if (nome.value == ""){
+        alert("Campo nome não pode estar em branco, escreve alguma coisa, animal.")
+    }
+    else{
+        alert("Ruivinha <3<3")
+    }
+}
